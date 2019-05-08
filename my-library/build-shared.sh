7046685e228a339a313594a33b28561ee2753782
zlargon <zlargon1988@gmail.com>
2019-05-08 15:53:35 -0400

    support Ubuntu and macOS

--- my-library/build-shared.sh
+++ my-library/build-shared.sh
@@ -6,4 +6,5 @@ mkdir -p ${dist}
 
 gcc -c -fPIC src/add.c -Iinclude/ -o ${dist}/add.o      # -fPIC?
 gcc -c -fPIC src/sub.c -Iinclude/ -o ${dist}/sub.o
-gcc -shared ${dist}/add.o ${dist}/sub.o -o ${dist}/libhello.so
+gcc -shared ${dist}/add.o ${dist}/sub.o -o ${dist}/libhello.so              # Ubuntu
+gcc -shared ${dist}/add.o ${dist}/sub.o -o $(pwd)/${dist}/libhello.dylib    # macOS absolute path?
