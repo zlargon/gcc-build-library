52905cc516281aa45680159ef954aa30b60db409
zlargon <zlargon1988@gmail.com>
2019-05-08 14:04:50 -0400

    [app] build app with static library

---
+++ my-app/build-static.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "[app] build app with static library"
+
+root=$(pwd)
+
+rm -rf out
+mkdir -p out
+
+cd ${root}
+gcc -c src/main.c -I../my-library/include -o out/main.o
+gcc out/main.o -L../my-library/out/static -lhello -o out/static-app.exe
---
+++ my-app/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hello.h"
+
+int main () {
+    printf("add(5, 3) = %d\n", add(5, 3));
+    printf("sub(5, 3) = %d\n", sub(5, 3));
+    return 0;
+}
